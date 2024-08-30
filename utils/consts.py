1dd307b18027e4531e692d34782cb6a4486726d7
levilightbeam <levi@lightbeam.ai>
2024-08-30 22:32:40 +0700

    update loading data function

--- utils/consts.py
+++ utils/consts.py
@@ -6,3 +6,6 @@ PRJ_PATH = Path(os.path.abspath(__file__)).resolve().parent.parent
 DATA_DIR = PRJ_PATH / "data"
 WEIGHT_DIR = PRJ_PATH / "weights"
 
+# Name of columns that is informative. We use these columns for training and evaluation.
+COLUMNS = ['Name (E)', 'YY/MM', 'NH3-N(㎎/L)', 'NO3-N(㎎/L)', 'PO4-P(㎎/L)', 'T-N(㎎/L)', 'T-P(㎎/L)',
+           'Dissolved Total N(㎎/L)', 'Dissolved Total P(㎎/L)', 'Hydrogen ion conc.', 'DO (㎎/L)', 'TSI(Chl-a)']
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple
 
 import pandas as pd
 
-from utils.consts import DATA_DIR
+from utils.consts import COLUMNS, DATA_DIR
 
 
 def load_data_from_excels(files: List[str], data_dir: Path = DATA_DIR, ) -> pd.DataFrame:
@@ -13,20 +13,11 @@ def load_data_from_excels(files: List[str], data_dir: Path = DATA_DIR, ) -> pd.D
     :param data_dir: Folder that contains the data files.
     :return: Dataframe.
     """
-    current_shape: Optional[Tuple[int, int]] = None
     current_columns: Optional[List[str]] = None
     dataframes: List[pd.DataFrame] = []
     for file in files:
         df = pd.read_excel(data_dir / file, header=0)
-        if current_shape and df.shape != current_shape:
-            raise ValueError(f"The shape of the dataframe is {df.shape} is missmatch with the current shape "
-                             f"{current_shape}")
-        else:
-            current_shape = df.shape
-        if current_columns and df.columns != current_columns:
-            raise ValueError(f"The columns of the dataframe is {df.columns} is missmatch with the current column "
-                             f"{current_columns}")
-        else:
-            current_columns = df.columns
+        assert set(COLUMNS).issubset(df.columns)
+        df = df[COLUMNS]
         dataframes.append(df)
     return pd.concat(dataframes)
