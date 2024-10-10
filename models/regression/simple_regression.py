021095d3d8a8c6ea5c7b40374ade98b7ea4fcc44
Quach Thai Duong <duong.qt210253@sis.hust.edu.vn>
2024-10-10 11:41:51 +0700

    update CNN

--- models/regression/simple_regression.py
+++ models/regression/simple_regression.py
@@ -24,3 +24,26 @@ class BasicRegression(BaseModel):
     def calculate_detailed_report(self, predictions: List[torch.Tensor], ground_truth: List[torch.Tensor],
                                   **kwargs) -> Dict[str, Any]:
         return {}
+
+class CNN(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.cnn = nn.Conv1d(in_channels = input_dim, out_channels=1, kernel_size=1)
+
+    def forward(self, batch):
+        batch = batch.unsqueeze(1)
+        batch = batch.permute(0, 2, 1)
+        batch = self.cnn(batch)
+        batch = batch.view(batch.size(0), -1)  
+        return batch
+    
+class BasicCNN(BaseModel):
+    def __init__(self, input_dim):
+        super().__init__(input_dim)
+
+    def _build_network(self):
+        return CNN(self.input_dim)
+    
+    def calculate_detailed_report(self, predictions: List[torch.Tensor], ground_truth: List[torch.Tensor],
+                                  **kwargs) -> Dict[str, Any]:
+        return {}
---
+++ tests/test_cnn.py
@@ -0,0 +1,22 @@
+from torch import nn
+from models.regression.simple_regression import BasicCNN
+from utils.data_utils import load_and_split_data
+from utils.dataset import get_simple_dataloader
+
+files = ["predata.xls", "Data.xlsx"]
+training_data, testing_data = load_and_split_data(files)
+train_dataloader = get_simple_dataloader(training_data, shuffle=True, batch_size = 4)
+test_dataloader = get_simple_dataloader(testing_data, shuffle=False, batch_size= 4)
+
+first_x, first_y = train_dataloader.dataset[0]
+
+input_dim = first_x.shape[-1]
+
+model = BasicCNN(input_dim = input_dim)
+
+num_epochs = 800
+lr = 0.01
+lr_params = {"start_factor": 0.1,
+             "end_factor": lr, "total_iters": num_epochs}
+model.fit(train_dataloader, test_dataloader, epochs=num_epochs, optimizer_type="adam", loss_fn=nn.L1Loss(), lr=lr,
+          scheduler_params=lr_params, patience=20)
